1653b800e94e6f900811b705ad7746a2f2cca2ad
Ben Chong <bensrww@gmail.com>
2022-02-17 00:43:20 +0800

    finish some methods

--- Board.java
+++ Board.java
@@ -4,37 +4,91 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
+    private int[][] boardTiles;
+    private int dimension;
+    private int hamming;
+    private int manhattan;
 
     // create a board from n-by-n array of tiles,
     // where tiles[row][col]] = tile at (row, col)
     public Board(int[][] tiles) {
-
+        boardTiles = tiles;
+        dimension = tiles.length;
+        hamming = hamming();
+        manhattan = manhattan();
     }
 
     // string representation of this board
     public String toString() {
-
+        System.out.println(dimension);
+        for (int row = 0; row < dimension; row += 1) {
+            for (int col = 0; col < dimension; col += 1) {
+                System.out.print(boardTiles[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 
     // number of tiles out of place
     public int hamming() {
-
+        int hammingDistance = 0;
+        for (int row = 0; row < dimension; row += 1) {
+            for (int col = 0; col < dimension; col += 1) {
+                final int targetPosition = row * dimension + col + 1;
+                if (boardTiles[row][col] != targetPosition && (
+                        row != dimension - 1 && col != dimension - 1
+                )) {
+                    hammingDistance += 1;
+                }
+            }
+        }
+        return hammingDistance;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-
+        int manhanttanDistance = 0;
+        for (int row = 0; row < dimension; row += 1) {
+            for (int col = 0; col < dimension; col += 1) {
+                final int targetPosition = row * dimension + col + 1;
+                final int currentElement = boardTiles[row][col];
+                if (currentElement != targetPosition && row != dimension - 1
+                        && col != dimension - 1) {
+                    final int targetRowIndex = currentElement / dimension;
+                    final int targetColIndex = currentElement % dimension - 1;
+                    manhanttanDistance += Math.abs(row - targetRowIndex);
+                    manhanttanDistance += Math.abs(col - targetColIndex);
+                }
+            }
+        }
+        return manhanttanDistance;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-
+        return hamming == 0;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        if (this.dimension != that.dimension) return false;
 
+        for (int row = 0; row < dimension; row +=1) {
+            for (int col = 0; col < dimension; col += 1) {
+                if (this.boardTiles[row][col] != that.boardTiles[row][col]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -49,6 +103,21 @@ public class Board {
 
     // unit testing (not graded)
     public static void main(String[] args) {
+        for (String filename : args) {
+            // read in the board specified in the filename
+            In in = new In(filename);
+            int n = in.readInt();
+            int[][] tiles = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tiles[i][j] = in.readInt();
+                }
+            }
+            // solve the slider puzzle
+            Board initial = new Board(tiles);
+            // Solver solver = new Solver(initial);
+            // StdOut.println(filename + ": " + solver.moves());
 
+        }
     }
 }
